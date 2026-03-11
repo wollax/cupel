aa8ce40fe9240813fc9d832feb1c1bb5febd66b6
Alexander Wollan <198976693+wollax@users.noreply.github.com>
2026-03-11 06:55:04 -0500

    test(03-01): add failing tests for RecencyScorer and PriorityScorer

---
+++ src/Wollax.Cupel/Scoring/PriorityScorer.cs
@@ -0,0 +1,9 @@
+namespace Wollax.Cupel.Scoring;
+
+public sealed class PriorityScorer : IScorer
+{
+    public double Score(ContextItem item, IReadOnlyList<ContextItem> allItems)
+    {
+        throw new NotImplementedException();
+    }
+}
