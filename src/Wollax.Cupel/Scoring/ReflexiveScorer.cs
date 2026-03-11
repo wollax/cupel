bcd1525c5b0fc769f2f2d7bd2f16bda27de2f8ca
Alexander Wollan <198976693+wollax@users.noreply.github.com>
2026-03-11 06:56:02 -0500

    test(03-01): add failing tests for ReflexiveScorer

---
+++ src/Wollax.Cupel/Scoring/ReflexiveScorer.cs
@@ -0,0 +1,9 @@
+namespace Wollax.Cupel.Scoring;
+
+public sealed class ReflexiveScorer : IScorer
+{
+    public double Score(ContextItem item, IReadOnlyList<ContextItem> allItems)
+    {
+        throw new NotImplementedException();
+    }
+}
